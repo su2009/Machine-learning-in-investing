aceaeb33f30359e254b13ddd61096d0c53f84cb6
su2009 <yangsu2009@gmail.com>
2017-01-20 04:28:46 +0800

    Update 0104_global_stats.py

--- 0104_global_stats.py
+++ 0104_global_stats.py
@@ -58,7 +58,10 @@ def test_run():
     symbols = ['SPY']
     df = get_data(symbols, dates)
   
-    # Compute Bollinger Bands
+
+
+# Compute Bollinger Bands my code 
+
     # 1. Compute rolling mean
     rm_SPY = get_rolling_mean(df['SPY'], window=20)
 
@@ -86,3 +89,71 @@ if __name__ == "__main__":
     
     
 #---- discussion on function rolling_mean https://discussions.udacity.com/t/rolling--is-deprecated/172494  
+
+# calculate daily returns = (day_n)/(day_n-1) - 1
+
+"""Compute daily returns."""
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+                parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df = df.join(df_temp)
+        if symbol == 'SPY':  # drop dates SPY did not trade
+            df = df.dropna(subset=["SPY"])
+
+    return df
+
+
+def plot_data(df, title="Stock prices", xlabel="Date", ylabel="Price"):
+    """Plot stock prices with a custom title and meaningful axis labels."""
+    ax = df.plot(title=title, fontsize=12)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.show()
+
+
+def compute_daily_returns(df):
+    """Compute and return the daily return values."""
+    # TODO: Your code here
+    # Note: Returned DataFrame must have the same number of rows
+    df_return = df.copy()
+    df_return[1:] = (df[1:]/df[:-1].value) - 1   # value is needed or python will match the index of df[1:] and df[:-1]
+    # df[1:] the second row to the last row , df[:-1] the first row to the second to last rwo
+    df_return.ix[0,:] = 0
+    return df_return
+  
+  # method 2
+  #def compute_daily_returns(df):
+  #  df_return = (df/df.shift(1)) - 1   # automatically replace nan with 0
+  #  return df_return
+
+def test_run():
+    # Read data
+    dates = pd.date_range('2012-07-01', '2012-07-31')  # one month only
+    symbols = ['SPY','XOM']
+    df = get_data(symbols, dates)
+    plot_data(df)
+
+    # Compute daily returns
+    daily_returns = compute_daily_returns(df)
+    plot_data(daily_returns, title="Daily returns", ylabel="Daily returns")
+
+
+if __name__ == "__main__":
+    test_run()
